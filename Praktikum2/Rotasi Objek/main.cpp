a39c058c7a44f3ec22e260140e91a54154d3ea94
baysix <bayualves6126@gmail.com>
2026-03-22 13:02:04 +0700

    Add files via upload

---
+++ Praktikum2/Rotasi Objek/main.cpp
@@ -0,0 +1,36 @@
+#include <GL/glut.h>
+
+void Segitiga(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLoadIdentity();
+
+    // translasi dulu
+    glTranslatef(0.25, -0.25, 0.0);
+
+    // rotasi 60 derajat terhadap sumbu Z
+    glRotatef(60.0, 0.0, 0.0, 1.0);
+
+    glBegin(GL_TRIANGLES);
+        glColor3f(1.0f, 0.0f, 0.0f); glVertex3f(-0.05,-0.05, 0.0);
+        glColor3f(0.0f, 1.0f, 0.0f); glVertex3f( 0.15,-0.05, 0.0);
+        glColor3f(0.0f, 0.0f, 1.0f); glVertex3f(-0.05, 0.05, 0.0);
+    glEnd();
+
+    glFlush();
+}
+
+int main(int argc, char* argv[])
+{
+    glutInit(&argc, argv);
+    glutInitWindowSize(640,480);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+    glutCreateWindow("Rotasi Segitiga");
+
+    glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+
+    glutDisplayFunc(Segitiga);
+    glutMainLoop();
+
+    return 0;
+}
